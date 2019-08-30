aca9378d746ed00a67a7baf98aabee190b90ffb0
Rehan Ayub <rehanayub20@gmail.com>
2019-08-30 16:08:18 -0400

    Reset doesn't work yet

--- TipCalculator/app.js
+++ TipCalculator/app.js
@@ -7,19 +7,39 @@ const amount = document.getElementById('amount')
 let result = document.querySelector('.result')
     , submitted = false;
 
-button.addEventListener('click', displayResult);
+button.addEventListener('click', submitted ? reset : displayResult);
 
 const calculateTipAmount = (amount, rating, numPeople) => {
-    return (parseFloat(amount.value) * (parseInt(rating.value,10)/100)/parseInt(numPeople.value,10)).toFixed(2);
+    const output = (parseFloat(amount.value) * (parseInt(rating.value,10)/100)/parseInt(numPeople.value,10)).toFixed(2);
+    return isNaN(output) ? '0.00' : output;
 };
 
 function displayResult(e) {
+
+    e.preventDefault();
+
     if (!submitted) {
-        e.preventDefault();
         result.innerHTML = `<h3>Tip Amount</h3>
                             <span>$ ${calculateTipAmount(amount,rating,numPeople)}</span>`;
+        
+        e.target.textContent = 'Reset';
     }
 
     submitted = true;
+}
+
+function reset (e) {
+
+    e.preventDefault();
+
+    if (submitted) {
+        e.target.textContent = 'Calculate';
+        amount.value = '';
+        rating.value = 10;
+        numPeople.value = 1;
+        result.innerHTML = '';
+    }
+
+    submitted = false;
 }
 
